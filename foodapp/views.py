5b32dc3f7c1592fd94776a17067c1a068a25b05c
Grootcode21 <antonyndichu15@gmail.com>
2024-05-09 22:35:55 -0700

    views

--- foodapp/views.py
+++ foodapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class FoodList(APIView):
+    def get(self, request):
+        food = Food.objects.all()
+        query = self.request.GET.get('search')
